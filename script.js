b4d474c9f28fddaab42d93d39e68dd1d45269d91
shahbas001 <shahbaskadavil@gmail.com>
2022-06-18 00:32:47 +0530

    script update

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-fetch('http://localhost:3000/getTimeStories')
-.then( res => res.json() )
-.then(response => {
- console.log(response);
- var mainContainer = document.getElementById("stories");
-       for (var i = 0; i < response.length; i++) {
-       //  var list = document.createElement("li");
-        // list.innerHTML = response[i.title]
-        var row = `
+fetch("http://localhost:3000/getTimeStories")
+  .then(res => res.json())
+  .then(response => {
+    var mainContainer = document.getElementById("stories");
+    for (var i = 0; i < response.length; i++) {
+      var row = `
         <li class="list">
         <a href=${response[i].link} target="_blank"> ${response[i].title}</a>
         </li>
-
-        `
-         mainContainer.innerHTML += row;
-         
-       }
- 
-
-  //  document.getElementById('testId').innerHTML = data[0][0][0];
-}) 
-.catch(error => console.error('Error:', error));
+        `;
+      mainContainer.innerHTML += row;
+    }
+  })
+  .catch(error => console.error("Error:", error));
